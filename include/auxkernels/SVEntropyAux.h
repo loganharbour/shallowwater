eca299d31533d7128ab8752ae1c5fa71523bc26d
Logan Harbour <loganhharbour@gmail.com>
2017-10-27 18:34:41 -0500

    Inheritance was a bad idea

--- include/auxkernels/SVEntropyAux.h
+++ include/auxkernels/SVEntropyAux.h
@@ -1,7 +1,7 @@
 #ifndef SVENTROPYAUX_H
 #define SVENTROPYAUX_H
 
-#include "SVAuxBase.h"
+#include "AuxKernel.h"
 
 // Forward Declarations
 class SVEntropyAux;
@@ -9,13 +9,23 @@ class SVEntropyAux;
 template <>
 InputParameters validParams<SVEntropyAux>();
 
-class SVEntropyAux : public SVAuxBase
+class SVEntropyAux : public AuxKernel
 {
 public:
   SVEntropyAux(const InputParameters & parameters);
 
 protected:
   virtual Real computeValue() override;
+
+  /// Coupled water height variable
+  const VariableValue & _h;
+
+  /// Coupled momentum variables
+  const VariableValue & _q_x;
+  const VariableValue & _q_y;
+
+  /// Constant of gravity
+  const Real _g;
 };
 
 #endif
